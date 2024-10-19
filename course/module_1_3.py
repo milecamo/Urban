321c976acb159d0396e139f5bcc429f7ab2f6720
milecamo <30475804+milecamo@users.noreply.github.com>
2024-10-20 01:55:43 +0300

    spaces removed

--- course/module_1_3.py
+++ course/module_1_3.py
@@ -1,26 +1,22 @@
 # someone's name
-
 name = "Роман"
 
 outputValue = name
 print("Name:", outputValue)
 
 # someone's age
-
 age = 41
 
 outputValue = age
 print("Age:", outputValue)
 
 # someone's new age
-
 age = age + 5
 
 outputValue = age
 print("New Age:", outputValue)
 
 # someone's student flag
-
 is_student = True
 
 outputValue = is_student
