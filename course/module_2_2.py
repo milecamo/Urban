7d9ba9246771608f1b8305bf1f8303b06b5f85bf
milecamo <30475804+milecamo@users.noreply.github.com>
2024-10-22 22:44:01 +0300

    module_2_2.py

---
+++ course/module_2_2.py
@@ -0,0 +1,12 @@
+print("Проверим сколько чисел равны между собой")
+first, second, third = (
+    map(int, input("Введите три целых числа через запятую: ").split(',')))
+
+if first == second == third:
+    print("Ответ: ",3)
+elif (first == second or
+      second == third or
+      first == third):
+    print("Ответ: ",2)
+else:
+    print("Ответ: ",0)
