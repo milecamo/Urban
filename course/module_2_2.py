a8b5d9b55d0eec18b9791de80c2f33b2a71c862e
milecamo <30475804+milecamo@users.noreply.github.com>
2024-10-22 23:05:21 +0300

    Update module_2_2.py
    Visual comprehension added

--- course/module_2_2.py
+++ course/module_2_2.py
@@ -3,10 +3,10 @@ first, second, third = (
     map(int, input("Введите три целых числа через запятую: ").split(',')))
 
 if first == second == third:
-    print("Ответ: ",3)
+    print("Ответ: ", 3)
 elif (first == second or
       second == third or
       first == third):
-    print("Ответ: ",2)
+    print("Ответ: ", 2)
 else:
-    print("Ответ: ",0)
+    print("Ответ: ", 0)
