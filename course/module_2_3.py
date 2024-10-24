1e123d5b4b5f5efb2dfb04308ef460bab2a1c0a8
milecamo <30475804+milecamo@users.noreply.github.com>
2024-10-24 22:56:16 +0300

    module_2_3.py

---
+++ course/module_2_3.py
@@ -0,0 +1,13 @@
+# Задача "Нули ничто, отрицание недопустимо!"
+
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+
+count = 0
+while count < my_list.__len__():
+    if my_list[count] < 0:
+        break
+    elif my_list[count] == 0:
+        count += 1
+        continue
+    print(my_list[count])
+    count += 1
