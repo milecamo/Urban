75a520b83fa7808371a72d66889fba74c3a634ba
milecamo <30475804+milecamo@users.noreply.github.com>
2024-10-27 23:19:59 +0300

    more comments

--- course/module_2_hard.py
+++ course/module_2_hard.py
@@ -25,11 +25,13 @@ def password(key):
     # first number is in range from 1 to key//2 (and one less if it's an even number)
     for i in range(1, key // 2 + key % 2):
         # sum of 2 numbers can't be greater than key
-        for j in range(i + 1, key - i + 1 ):
+        # (key - i) + i = key
+        for j in range(i + 1, (key - i) + 1):
             # add numbers to result
             if key % (i + j) == 0:
                 result += f"{i}{j}"
     return result
+
 
 # lets check result with check_list
 for k in range(3, 21):
