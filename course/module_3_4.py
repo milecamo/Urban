c6d84d1c825992ea108c7a0b5c9132a63292a3e6
milecamo <30475804+milecamo@users.noreply.github.com>
2024-11-08 00:30:34 +0300

    module_3_4.py you want it you get it

--- course/module_3_4.py
+++ course/module_3_4.py
@@ -3,10 +3,10 @@
 def single_root_words(root_word, *other_words):
     same_words = []
     root_word = root_word.lower()
-    for i in range(len(other_words)):
-        other_word = other_words[i].lower()
-        if other_word in root_word or root_word in other_word:
-            same_words.append(other_words[i])
+    for word in other_words:
+        low_word = word.lower()
+        if low_word in root_word or root_word in low_word:
+            same_words.append(word)
     return same_words
 
 
