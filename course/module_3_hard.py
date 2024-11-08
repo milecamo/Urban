918be5ca2592827ff507f1a0de76d214ea554938
milecamo <30475804+milecamo@users.noreply.github.com>
2024-11-09 00:35:52 +0300

    module_3_hard.py One, two, three, four, five .... Isn't that everything?

--- course/module_3_hard.py
+++ course/module_3_hard.py
@@ -8,17 +8,28 @@ data_structure = [
     ((), [{(2, 'Urban', ('Urban2', 35))}])
 ]
 
+res_str = ""
+
+
+def add_str(str):
+    global res_str
+    if not res_str:
+        res_str = f'{str}'
+    else:
+        res_str += f'+{str}'
+
 
 def calculate_structure_sum(*args):
     result = 0
     for i in args:
         if isinstance(i, int):
             result += i
+            add_str(i)
         elif isinstance(i, str):
             result += len(i)
+            add_str(f"len('{i}')")
         elif isinstance(i, dict):
-            result += calculate_structure_sum(*i.keys())
-            result += calculate_structure_sum(*i.values())
+            result += calculate_structure_sum(*i.items())
         else:
             result += calculate_structure_sum(*i)
     return result
@@ -26,3 +37,4 @@ def calculate_structure_sum(*args):
 
 result = calculate_structure_sum(data_structure)
 print(result)
+print(f'{res_str}={result}')
