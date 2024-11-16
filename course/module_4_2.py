6967c13bc845fa0fc258fff772f66d3fa9e8fd06
milecamo <30475804+milecamo@users.noreply.github.com>
2024-11-16 23:26:09 +0300

    module_4_2.py "Namespace"

---
+++ course/module_4_2.py
@@ -0,0 +1,11 @@
+# "Пространство имен
+
+def test_function():
+    def inner_function():
+        print("Я в области видимости функции test_function")
+
+    inner_function()
+
+
+# NameError: name 'inner_function' is not defined
+inner_function()
