08ace574d41822b0127330771e523cad8ba076dd
milecamo <30475804+milecamo@users.noreply.github.com>
2024-11-18 02:37:29 +0300

    module_4_hard.py Best Time to Buy and Sell Stock

--- course/module_4_hard.py
+++ course/module_4_hard.py
@@ -8,16 +8,28 @@ def profit(*prices):
         if not profit > 0:
             profit = 0
         profits.append(profit)
-    return max(profits)
+    prft = max(profits)
+    buy_on = -1
+    sell_on = -1
+    if prft:
+        buy_on = profits.index(prft)
+        sell_on = prices.index(prices[buy_on - 1] + prft, buy_on) + 1
+    return prft, buy_on, sell_on
 
 
 def print_profit(prices, should_be):
-    prft = profit(*prices)
-    print(f'Maximum profit for stock prices {prices} is {prft}', prft == should_be)
+    prft, buy_on, sell_on = profit(*prices)
+    if prft:
+        print(f'Maximum profit for stock prices {prices} is {prft}', prft == should_be,
+              f'Buy on day {buy_on}, sell on day {sell_on}')
+    else:
+        print(f'Maximum profit for stock prices {prices} is {prft}', prft == should_be,
+              "Don't buy")
 
 
 print_profit((7, 1, 5, 3, 6, 4), 5)
 print_profit((7, 6, 4, 3, 1), 0)
+print_profit((7,8), 1)
 print_profit((7,), 0)
 print_profit((), 0)
-print(f'Maximum profit for empty stock prices is {profit()}')
+print(f'Maximum profit for empty stock prices is {profit()[0]}')
