43903a8c44c1d9a9c38653be09a4d091be416dee
milecamo <30475804+milecamo@users.noreply.github.com>
2024-11-18 02:09:43 +0300

    module_4_hard.py Best Time to Buy and Sell Stock

--- course/module_4_hard.py
+++ course/module_4_hard.py
@@ -1,2 +1,23 @@
 # Best Time to Buy and Sell Stock
 
+
+def profit(*prices):
+    profits = [0]
+    for i in range(len(prices) - 1):
+        profit = max(prices[i + 1:len(prices)]) - prices[i]
+        if not profit > 0:
+            profit = 0
+        profits.append(profit)
+    return max(profits)
+
+
+def print_profit(prices, should_be):
+    prft = profit(*prices)
+    print(f'Maximum profit for stock prices {prices} is {prft}', prft == should_be)
+
+
+print_profit((7, 1, 5, 3, 6, 4), 5)
+print_profit((7, 6, 4, 3, 1), 0)
+print_profit((7,), 0)
+print_profit((), 0)
+print(f'Maximum profit for empty stock prices is {profit()}')
