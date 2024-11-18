b2b2e8167fc12fdafe189540df41f0776abe45fe
milecamo <30475804+milecamo@users.noreply.github.com>
2024-11-19 02:57:37 +0300

    module_4_hard.py Best Time to Buy and Sell Stock

--- course/module_4_hard.py
+++ course/module_4_hard.py
@@ -15,9 +15,9 @@ def profit(*prices):
 
 
 def print_profit(prices, should_be, should_buy_on=-1, should_sell_on=-1):
-    result = max_profit, buy_on, sell_on = profit(*prices)
-    qa_result = result == (should_be, should_buy_on, should_sell_on)
-    print(f'Profit for stock prices {prices} was {max_profit} ', qa_result)
+    max_profit, buy_on, sell_on = profit(*prices)
+    print(f'Profit for stock prices {prices} was {max_profit} ',
+          (max_profit, buy_on, sell_on) == (should_be, should_buy_on, should_sell_on))
     if max_profit:
         print(f'The best time to buy was on day {buy_on} (price = {prices[buy_on - 1]}),')
         print(f'and to sell on day {sell_on} (price = {prices[sell_on - 1]}), '
