b6cdb7245fb9b8e180a881cf0862145fa5564495
milecamo <30475804+milecamo@users.noreply.github.com>
2024-11-19 02:54:26 +0300

    module_4_hard2.py Best Time to Buy and Sell Stock II

--- course/module_4_hard2.py
+++ course/module_4_hard2.py
@@ -1,15 +1,15 @@
 # Best Time to Buy and Sell Stock II
 # https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii
 
-def up_trend(prices, index=0):
-    for i in range(index, len(prices) - 1):
+def up_trend(prices, StartIndex):
+    for i in range(StartIndex, len(prices) - 1):
         if prices[i] < prices[i + 1]:
             return i + 1
     return 0
 
 
-def down_trend(prices, index=0):
-    for i in range(index, len(prices) - 1):
+def down_trend(prices, StartIndex):
+    for i in range(StartIndex, len(prices) - 1):
         if prices[i] > prices[i + 1]:
             return i + 1
     return 0
@@ -24,17 +24,13 @@ def max_profit(*prices):
         up_day = up_trend(prices, index)
         if up_day:
             buy_on.append(up_day)
-            down_day = down_trend(prices, up_day - 1)
-            if down_day:
-                sell_on.append(down_day)
-                profit += prices[down_day - 1] - prices[up_day - 1]
-                index = down_day
-            else:
-                sell_on.append(len(prices))
-                profit += prices[len(prices) - 1] - prices[up_day - 1]
+            index = down_trend(prices, up_day - 1)
+            if not index:
                 index = len(prices)
+            sell_on.append(index)
+            profit += prices[index - 1] - prices[up_day - 1]
         else:
-            index = len(prices)
+            break
 
     return profit, buy_on, sell_on
 
@@ -44,11 +40,9 @@ def print_profit(prices, should_be, should_buy_on=None, should_sell_on=None):
         should_buy_on = []
     if not should_sell_on:
         should_sell_on = []
-    result = profit, buy_on, sell_on = max_profit(*prices)
-    # print(should_buy_on, should_sell_on)
-    # print(buy_on, sell_on)
-    qa_result = result == (should_be, should_buy_on, should_sell_on)
-    print(f'Maximum profit for stock prices {prices} was {profit} ', qa_result)
+    profit, buy_on, sell_on = max_profit(*prices)
+    print(f'Maximum profit for stock prices {prices} was {profit} ',
+          (profit, buy_on, sell_on) == (should_be, should_buy_on, should_sell_on))
     if profit:
         for i in range(len(buy_on)):
             beg_str = "The"
