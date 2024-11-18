2585619a1e4c67909b87ec615d98931a8ce84aa7
milecamo <30475804+milecamo@users.noreply.github.com>
2024-11-18 04:04:39 +0300

    module_4_2.py Namespace

--- course/module_4_2.py
+++ course/module_4_2.py
@@ -9,3 +9,7 @@ def test_function():
 
 # NameError: имя 'inner_function' не определено
 inner_function()
+
+# !!!
+# Please see module_4_hard.py Best Time to Buy and Sell Stock
+# https://github.com/milecamo/Urban/blob/master/course/module_4_hard.py
