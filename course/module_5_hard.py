66b989ebcb235b8f067280d5c7bad7de93bb5bce
milecamo <30475804+milecamo@users.noreply.github.com>
2024-12-02 08:01:50 +0300

    module_5_hard.py Your own YouTube

--- course/module_5_hard.py
+++ course/module_5_hard.py
@@ -19,10 +19,10 @@ class User:
 
 
 class Video:
-    def __init__(self, title, duration, adult_mode=False):
+    def __init__(self, title, duration, time_now=0, adult_mode=False):
         self.title = title
         self.duration = duration
-        self.time_now = 0
+        self.time_now = time_now
         self.adult_mode = adult_mode
 
     def __eq__(self, other):
