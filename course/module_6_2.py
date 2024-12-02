b9158e8b1bd438ff0840af07c178e6b1be2ff7cf
milecamo <30475804+milecamo@users.noreply.github.com>
2024-12-02 07:02:57 +0300

    module_6_2.py Modify cannot receive

--- course/module_6_2.py
+++ course/module_6_2.py
@@ -1,7 +1,7 @@
 # Задача "Изменять нельзя получать"
 
 class Vehicle:
-    __COLOR_VARIANTS = ['red', 'blue', 'green', 'black', 'yellow']
+    __COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
 
     def __init__(self, owner, model, color, engine_power):
         self.owner = owner
@@ -10,13 +10,13 @@ class Vehicle:
         self.__color = color
 
     def get_model(self):
-        return f"Модель: {self._Vehicle__model}"
+        return f"Модель: {self.__model}"
 
     def get_horsepower(self):
-        return f"Мощность двигателя: {self._Vehicle__engine_power}"
+        return f"Мощность двигателя: {self.__engine_power}"
 
     def get_color(self):
-        return f"Цвет: {self._Vehicle__color}"
+        return f"Цвет: {self.__color}"
 
     def print_info(self):
         print(self.get_model())
@@ -25,8 +25,8 @@ class Vehicle:
         print(f"Владелец: {self.owner}")
 
     def set_color(self, new_color):
-        if new_color.lower() in self._Vehicle__COLOR_VARIANTS:
-            self._Vehicle__color = new_color
+        if new_color.lower() in self.__COLOR_VARIANTS:
+            self.__color = new_color
         else:
             print(f"Нельзя сменить цвет на {new_color}")
 
