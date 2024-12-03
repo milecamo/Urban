2c4cc5a211f686fe4ee0f75848461969a61c9b4b
milecamo <30475804+milecamo@users.noreply.github.com>
2024-12-03 05:18:52 +0300

    Delete course/digits.py

--- course/digits.py
+++
@@ -1,8 +0,0 @@
-vals = ("038", "੦੩੮", "０３８",
-        "⁰³⁸", "⒊⒏", "⓪③⑧",
-        "↉⅛⅘", "ⅠⅢⅧ", "⑩⑬㊿", "壹貳參",
-        "38.0", "-38"
-        )
-
-for s in vals:
-    print(s, s.isnumeric(), s.isdigit(), s.isdecimal())
