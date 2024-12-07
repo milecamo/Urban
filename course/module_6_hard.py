c7e93b8da8130928dc050c1f2ba1385ef5886fcf
milecamo <30475804+milecamo@users.noreply.github.com>
2024-12-07 07:46:48 +0300

    module_6_hard.py They're all so similar

--- course/module_6_hard.py
+++ course/module_6_hard.py
@@ -37,7 +37,6 @@ class Figure:
     def __is_valid_sides(self, *sides):
         if len(sides) != self.sides_count:
             return False
-
         for side in sides:
             if not isinstance(side, int) or side <= 0:
                 return False
@@ -55,7 +54,6 @@ class Figure:
     def set_sides(self, *new_sides):
         if self.__is_valid_sides(*new_sides):
             self.__sides = list(new_sides)
-
         if not self.__sides:
             self.__sides = []
             for i in range(self.sides_count):
@@ -67,20 +65,22 @@ class Circle(Figure):
 
     def __init__(self, color=DEFAULT_COLOR, *sides):
         # although we do not must define self.__radius in __init__
+        # it could be defined in set_sides()
         self.__radius = 0
         super().__init__(color, *sides)
 
     def set_sides(self, *new_sides):
+        old_side = 0 if not hasattr(self, '__radius') or not self.__radius else self.get_sides()[0]
         # if self.__radius isn't defined in __init__
         # old_side = 0 if not hasattr(self, '__radius') else self.get_sides()[0]
-        old_side = 0 if not self.__radius else self.get_sides()[0]
 
         super().set_sides(*new_sides)
-
         new_side = self.get_sides()[0]
+
         if old_side != new_side:
-            # for the 1st time self.__radius will be defined here
             # if it's not defined in __init__
+            # self.__radius will be defined here
+            # for the 1st time
             self.__radius = new_side / 2 / pi
 
     def get_square(self):
@@ -106,11 +106,10 @@ class Cube(Figure):
         self.sides_count = 1
         super().set_sides(*new_sides)
         self.sides_count = 12
-
+        # then append 11 more equal sides
         sides = self.get_sides()
         for i in range(self.sides_count - 1):
             sides.append(sides[0])
-
         super().set_sides(*sides)
 
     def get_volume(self):
@@ -119,8 +118,10 @@ class Cube(Figure):
     def get_square(self):
         return self.get_sides()[0] ** 2 * 6
 
-    # площадь куба
     # def __len__(self):
+    #     # сумма ребер куба
+    #     return self.get_sides()[0] * self.sides_count
+    #     # площадь куба
     #     return self.get_square()
 
 
