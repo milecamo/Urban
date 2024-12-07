df70cc4821898fb60d52b53c1e7eb621c1a9d920
milecamo <30475804+milecamo@users.noreply.github.com>
2024-12-07 09:24:28 +0300

    webinars

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 # pip install simple_draw
 # pip install termcolor
 python3 ./course/module_6_hard.py 
-# python3 ./webinars/3_module/02_snowflake.py 
+# python3 ./course/webinars/3_module/02_snowflake.py 
