904fc99de9fe6b5537ccb3ea128ffe27aaa78af3
milecamo <30475804+milecamo@users.noreply.github.com>
2024-12-09 08:42:36 +0300

    module_7_2.py Write down and remember

--- course/module_7_2/module_7_2.py
+++ course/module_7_2/module_7_2.py
@@ -4,8 +4,8 @@ def custom_write(file_name, strings):
     strings_positions = {}
     file = open(file_name, 'w', encoding='utf-8')
     for i in range(len(strings)):
-        strings_positions[(i+1,file.tell())] = strings[i]
-        file.write(strings[i]+'\n')
+        strings_positions[(i + 1, file.tell())] = strings[i]
+        file.write(strings[i] + '\n')
     file.close()
     return strings_positions
 
@@ -15,8 +15,8 @@ info = [
     'Используйте кодировку utf-8.',
     'Because there are 2 languages!',
     'Спасибо!'
-    ]
+]
 
 result = custom_write('test.txt', info)
 for elem in result.items():
-  print(elem)
+    print(elem)
