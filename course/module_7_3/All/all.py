cb0a29b96fc2ea00e646b37bedab1f501c721ab6
milecamo <30475804+milecamo@users.noreply.github.com>
2024-12-10 13:10:29 +0300

    module_7_3.py Will find it everywhere

--- course/module_7_3/All/all.py
+++
@@ -1,4 +0,0 @@
-from ..module_7_3 import WordsFinder
-
-if __name__ == '__main__':
-    pass
---
+++ course/module_7_3/all.py
@@ -0,0 +1,9 @@
+from ..module_7_3 import WordsFinder
+
+if __name__ == '__main__':
+    finder1 = WordsFinder('Walt Whitman - O Captain! My Captain!.txt',
+                          'Rudyard Kipling - If.txt',
+                          'Mother Goose - Monday’s Child.txt')
+    print(finder1.get_all_words())
+    print(finder1.find('the'))
+    print(finder1.count('the'))
