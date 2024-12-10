6ed47a5005789ef94b3945f4183526bf3e45dbb9
milecamo <30475804+milecamo@users.noreply.github.com>
2024-12-10 12:54:26 +0300

    module_7_3.py Will find it everywhere

---
+++ course/module_7_3/All/all.py
@@ -0,0 +1,4 @@
+from ..module_7_3 import WordsFinder
+
+if __name__ == '__main__':
+    pass
--- course/module_7_3/module_7_3.py
+++ course/module_7_3/module_7_3.py
@@ -39,7 +39,8 @@ class WordsFinder:
             word_counts[file_name] = word_count
         return word_counts
 
-finder2 = WordsFinder('test_file.txt')
-print(finder2.get_all_words())  # Все слова
-print(finder2.find('TEXT'))  # 3 слово по счёту
-print(finder2.count('teXT'))  # 4 слова teXT в тексте всего
+if __name__ == '__main__':
+    finder2 = WordsFinder('test_file.txt')
+    print(finder2.get_all_words())  # Все слова
+    print(finder2.find('TEXT'))  # 3 слово по счёту
+    print(finder2.count('teXT'))  # 4 слова teXT в тексте всего
