307645cf6d9a8e218d02c883c117a83f51998b58
milecamo <30475804+milecamo@users.noreply.github.com>
2024-12-12 20:26:24 +0300

    module_7_5.py Files in the operating system

---
+++ course/module_7_5.py
@@ -0,0 +1,16 @@
+# Задача "Файлы в операционной системе"
+
+import os
+import time
+
+directory = '.'
+for root, dirs, files in os.walk(directory):
+    for file in files:
+        filepath = os.path.join(os.getcwd(), "" if len(root)==1 else root[2:], file)
+        filetime = os.path.getmtime(filepath)
+        formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
+        filesize = os.path.getsize(filepath)
+        parent_dir = os.path.dirname(filepath)
+        print(f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize} байт, Время изменения: {formatted_time}, '
+              f'Родительская директория: {parent_dir}')
+        # os.system("pause")
