b974e84d34d1812b43e9f52efc0c3806975933f5
milecamo <30475804+milecamo@users.noreply.github.com>
2024-12-12 21:38:55 +0300

    module_7_5.py Files in the operating system

--- course/module_7_5.py
+++ course/module_7_5.py
@@ -13,4 +13,4 @@ for root, dirs, files in os.walk(directory):
         parent_dir = os.path.dirname(filepath)
         print(f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize} байт, Время изменения: {formatted_time}, '
               f'Родительская директория: {parent_dir}')
-        # os.system("pause")
+    os.system("pause")
