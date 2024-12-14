0257aedf48bffd33fd568e65b635a71fed8cdb67
milecamo <30475804+milecamo@users.noreply.github.com>
2024-12-15 00:47:46 +0300

    module_7_5.py Files in the operating system

--- course/module_7_5.py
+++ course/module_7_5.py
@@ -6,7 +6,8 @@ import time
 directory = '.'
 for root, dirs, files in os.walk(directory):
     for file in files:
-        filepath = os.path.join(os.getcwd(), root[2:], file)
+        filepath = os.path.join(os.getcwd(), root, file)
+        # filepath = os.path.join(os.getcwd(), root[2:], file) # for windows
         filetime = os.path.getmtime(filepath)
         formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
         filesize = os.path.getsize(filepath)
