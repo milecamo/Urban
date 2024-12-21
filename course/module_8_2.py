81cba1f07ceaa9e6c5b622ae2a1383b97d817f5b
milecamo <30475804+milecamo@users.noreply.github.com>
2024-12-22 00:16:49 +0300

    module_8_2.py Intercept Plan

--- course/module_8_2.py
+++ course/module_8_2.py
@@ -14,7 +14,7 @@ def personal_sum(numbers):
 
 
 def calculate_average(numbers):
-    if not isinstance(numbers, (list, tuple, str)):
+    if not isinstance(numbers, (list, tuple)):
         print('В numbers записан некорректный тип данных')
         return None
     sum, incorrect_data = personal_sum(numbers)
@@ -24,7 +24,7 @@ def calculate_average(numbers):
         return 0
 
 
-print(f'Результат 1: {calculate_average("1, 2, 3")}')  # Строка перебирается, но каждый символ - строковый тип
+print(f'Результат 1: {calculate_average(("1",","," ","2",","," ","3"))}')  # Кортеж перебирается, но каждый символ - строковый тип
 print(f'Результат 2: {calculate_average([1, "Строка", 3, "Ещё Строка"])}')  # Учитываются только 1 и 3
 print(f'Результат 3: {calculate_average(567)}')  # Передана не коллекция
 print(f'Результат 4: {calculate_average([42, 15, 36, 13])}')  # Всё должно работать
