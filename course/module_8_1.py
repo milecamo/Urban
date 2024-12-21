e351241bc35d40a11980d8987c57544d7664f15a
milecamo <30475804+milecamo@users.noreply.github.com>
2024-12-21 23:43:23 +0300

    module_8_1.py Programmers can do anything

--- course/module_8_1.py
+++ course/module_8_1.py
@@ -1,10 +1,13 @@
 # Задание "Программистам всё можно"
 
 def add_everything_up(a, b):
+    if ((isinstance(a, str) and isinstance(b, (int, float))) or
+            (isinstance(a, (int, float)) and isinstance(b, str))):
+        return f"{a}{b}"
     try:
         return a + b
     except TypeError:
-        return str(a) + str(b)
+        pass
 
 
 print(add_everything_up(123.456, 'строка'))
