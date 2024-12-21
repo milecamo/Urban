df46ca437d4fecda7df0aa2dbeac9f701f9e9a0c
milecamo <30475804+milecamo@users.noreply.github.com>
2024-12-22 00:03:50 +0300

    module_8_2.py Intercept Plan

--- course/module_8_2.py
+++ course/module_8_2.py
@@ -5,22 +5,23 @@ def personal_sum(numbers):
     result = 0
     incorrect_data = 0
     for i in numbers:
-        try:
+        if isinstance(i, (int, float)):
             result += i
-        except TypeError:
+        else:
             incorrect_data += 1
             print(f"Некорректный тип данных для подсчёта суммы - {i}")
     return result, incorrect_data
 
 
 def calculate_average(numbers):
-    try:
-        sum, incorrect_data = personal_sum(numbers)
-        return sum / (len(numbers) - incorrect_data)
-    except ZeroDivisionError:
-        return 0
-    except TypeError:
+    if not isinstance(numbers, (list, tuple, str)):
         print('В numbers записан некорректный тип данных')
+        return None
+    sum, incorrect_data = personal_sum(numbers)
+    if len(numbers) - incorrect_data > 0:
+        return sum / (len(numbers) - incorrect_data)
+    else:
+        return 0
 
 
 print(f'Результат 1: {calculate_average("1, 2, 3")}')  # Строка перебирается, но каждый символ - строковый тип
