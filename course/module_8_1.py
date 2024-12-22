dc11577abfbd56f1a1cc1f3cea5e4ec85c351a4f
milecamo <30475804+milecamo@users.noreply.github.com>
2024-12-22 22:52:48 +0300

    module_8_1.py Programmers can do anything

--- course/module_8_1.py
+++ course/module_8_1.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 # Задание "Программистам всё можно"
 
+# may be more readable
+# if (isinstance(a, (str, int, float)) and isinstance(b, (str, int, float)) and type(a) != type(b) and
+#         not (isinstance(a, (int, float)) and isinstance(b, (int, float)))):
 def add_everything_up(a, b):
     if ((isinstance(a, str) and isinstance(b, (int, float))) or
             (isinstance(a, (int, float)) and isinstance(b, str))):
