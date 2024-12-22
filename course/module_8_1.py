2518215a7ba38a2f8a72d7786eeac8bd929b3967
milecamo <30475804+milecamo@users.noreply.github.com>
2024-12-22 03:49:11 +0300

    module_8_1.py Programmers can do anything

--- course/module_8_1.py
+++ course/module_8_1.py
@@ -1,15 +1,16 @@
+# -*- coding: utf-8 -*-
 # Задание "Программистам всё можно"
 
 def add_everything_up(a, b):
     if ((isinstance(a, str) and isinstance(b, (int, float))) or
             (isinstance(a, (int, float)) and isinstance(b, str))):
         return f"{a}{b}"
-    try:
-        return a + b
-    except TypeError:
-        pass
+    return a + b
 
 
-print(add_everything_up(123.456, 'строка'))
-print(add_everything_up('яблоко', 4215))
-print(add_everything_up(123.456, 7))
+try:
+    print(add_everything_up(123.456, 'строка'))
+    print(add_everything_up('яблоко', 4215))
+    print(add_everything_up(123.456, 7))
+except TypeError as exc:
+    print(type(exc), exc)
