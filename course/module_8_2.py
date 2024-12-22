c7abcfe298517267ed202f9eb2bb5cb0223a893e
milecamo <30475804+milecamo@users.noreply.github.com>
2024-12-22 03:54:39 +0300

    module_8_2.py Intercept Plan

--- course/module_8_2.py
+++ course/module_8_2.py
@@ -26,9 +26,22 @@ def calculate_average(numbers):
 
 
 try:
-    print(f'Результат 1: {calculate_average(tuple("1, 2, 3"))}')  # Строка перебирается, но каждый символ - строковый тип
+    print(
+        f'Результат 1: {calculate_average(tuple("1, 2, 3"))}')  # Строка перебирается, но каждый символ - строковый тип
+except (TypeError, ZeroDivisionError) as exc:
+    print(type(exc), exc)
+
+try:
     print(f'Результат 2: {calculate_average([1, "Строка", 3, "Ещё Строка"])}')  # Учитываются только 1 и 3
+except (TypeError, ZeroDivisionError) as exc:
+    print(type(exc), exc)
+
+try:
     print(f'Результат 3: {calculate_average(567)}')  # Передана не коллекция
+except (TypeError, ZeroDivisionError) as exc:
+    print(type(exc), exc)
+
+try:
     print(f'Результат 4: {calculate_average([42, 15, 36, 13])}')  # Всё должно работать
 except (TypeError, ZeroDivisionError) as exc:
     print(type(exc), exc)
