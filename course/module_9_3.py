5c61227061bd9c725fd59a343c990e56ac9f3fcd
milecamo <30475804+milecamo@users.noreply.github.com>
2024-12-26 03:39:53 +0300

    module_9_3.py Generator assemblies

---
+++ course/module_9_3.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Задание по теме "Генераторные сборки"
+
+first = ['Strings', 'Student', 'Computers']
+second = ['Строка', 'Урбан', 'Компьютер']
+
+first_result = (abs(len(x[0]) - len(x[1])) for x in zip(first, second) if len(x[0]) != len(x[1]))
+
+second_result = (len(first[i]) == len(second[i]) for i in range(min(len(first), len(second))))
+
+print(list(first_result))
+print(list(second_result))
