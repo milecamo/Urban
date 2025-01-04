da2852cd29906a1004a9c404329d0d61fd85fa69
milecamo <30475804+milecamo@users.noreply.github.com>
2025-01-04 08:21:24 +0300

    module_10_2.py For honor and valor!

--- course/module_10/module_10_2.py
+++ course/module_10/module_10_2.py
@@ -1,3 +1,37 @@
 # -*- coding: utf-8 -*-
 # Задача "За честь и отвагу!"
 
+from threading import Thread
+from time import sleep
+
+
+class Knight(Thread):
+    def __init__(self, name: str, power: int):
+        Thread.__init__(self)
+        self.name = name
+        self.power = power
+
+    def run(self):
+        print(f"{self.name}, на нас напали!\n", end="")
+        warriors = 100
+        days = 0
+        while warriors > 0:
+            sleep(1)
+            days += 1
+            warriors = 0 if warriors <= self.power else warriors - self.power
+            print(f"{self.name} сражается {days} день(дня)..., осталось {warriors} воинов.\n", end="")
+        print(f"{self.name} одержал победу спустя {days} дней(дня)!\n", end="")
+
+
+# Создание класса
+first_knight = Knight('Sir Lancelot', 10)
+second_knight = Knight("Sir Galahad", 20)
+
+# Запуск потоков и остановка текущего
+first_knight.start()
+second_knight.start()
+first_knight.join()
+second_knight.join()
+
+# Вывод строки об окончании сражения
+print("Все битвы закончились!")
