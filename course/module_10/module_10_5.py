ac3559a2d1be8deeecc84ff8535f638ac2b74475
milecamo <30475804+milecamo@users.noreply.github.com>
2025-01-09 01:25:42 +0300

    module_10_5.py Multiprocess reading

--- course/module_10/module_10_5.py
+++ course/module_10/module_10_5.py
@@ -14,18 +14,18 @@ def read_info(name):
             all_data.append(line)
 
 
-filenames = [f'./file {number}.txt' for number in range(1, 5)]
-
-# Линейный вызов 0:00:05.078704 (линейный)
-# linecall_start = datetime.now()
-# for filename in filenames:
-#     read_info(filename)
-# print(f'{datetime.now() - linecall_start} (линейный)\n', end='')
-
-# Многопроцессный 0:00:08.392741 (многопроцессный)
 if __name__ == '__main__':
+    filenames = [f'./file {number}.txt' for number in range(1, 5)]
+
+    # Линейный вызов 0:00:05.078704 (линейный)
+    linecall_start = datetime.now()
+    for filename in filenames:
+        read_info(filename)
+    print(f'{datetime.now() - linecall_start} (линейный)\n', end='')
+
+    # Многопроцессный 0:00:08.392741 (многопроцессный)
     time_start = datetime.now()
     for filename in filenames:
-        with Pool(5) as p:
+        with Pool() as p:
             p.map(read_info, filenames)
     print(f'{datetime.now() - time_start} (многопроцессный)\n', end='')
